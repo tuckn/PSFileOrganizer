f57cbf69c843a971112d8ab60439f99ba7ca91d0
tuckn <tuckn333@gmail.com>
2022-05-24 05:39:22 +0900

    Fix that cannot read path including "&"

--- RenameFileIntoDateCode/AddDateCodeIso8601JpToPrefix.cmd
+++ RenameFileIntoDateCode/AddDateCodeIso8601JpToPrefix.cmd
@@ -1,11 +1,15 @@
 @ECHO OFF
 
-SET FilePath=%~1
-IF "%FilePath%"=="" (SET /P FilePath="Input the full path: ")
+REM FIXME Can not read a path including "&"
+REM SET FilePath=%~1
+REM IF "%FilePath%"=="" (SET /P FilePath="Input the full path: ")
+
+SET FilePath=%1
+REM ECHO %FilePath%
 
 SET PS1_PATH=%~dp0\Run.ps1
 @ECHO ON
-powershell -ExecutionPolicy Bypass -File "%PS1_PATH%" -FilePath "%FilePath%" -DateFormat "yyyyMMddThhmmss+0900_" -OriginalName "Post"
+powershell -ExecutionPolicy Bypass -File "%PS1_PATH%" -FilePath %FilePath% -DateFormat "yyyyMMddThhmmss+0900_" -OriginalName "Post"
 
 @ECHO OFF
 SET FilePath=
